8a1e42a82cc13352ecdcc4c65cd884b1991f89f9
Oleg Krupenya <taipan90@rambler.ru>
2014-07-30 23:56:51 +0300

    navbar for the search results

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,16 @@ function init() {
 	var homeNavBar = document.getElementById('homeNavBar');
 	var btnSearch = document.getElementById('btnSearch');
 	var mainContainer = document.getElementById('mainContainer');
+	var resultsNavBar = document.getElementById('resultsNavBar');
 	searchResults.style.display = "none";
+    resultsNavBar.style.display = "none";
 	btnSearch.addEventListener('click', doSearch, false);
 }
 
 function doSearch(event) {
 	event.preventDefault();
 	searchResults.style.display = "block";
+    resultsNavBar.style.display = "block";
 	mainContainer.style.display = "none";
 	homeNavBar.style.display = "none";
 }
